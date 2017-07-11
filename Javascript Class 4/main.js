0a4a9cb50a2769a72dd08e63e5fa1e04dc5c44fb
Radha <Radha4736@gmail.com>
2017-07-11 23:11:51 +0530

    imported dataTables and CurrentSongDetails

--- Javascript Class 4/main.js
+++ Javascript Class 4/main.js
@@ -1,14 +1,12 @@
+		var currentSongNumber = 1;
+		var willLoop = 0;
+		var willShuffle = 0;
 	
-function validate() {
-  if(myform.fname.value.length==3)
-  {
-document.getElementById('errfn').innerHTML="Name should be greater than 3";
-  }
- }
-
-
-	
-			
+		function randomExcluded(min, max, excluded) {
+    var n = Math.floor(Math.random() * (max-min) + min);
+    if (n >= excluded) n++;
+    return n;
+}	
 	
 	
 	
@@ -25,6 +23,11 @@ document.getElementById('errfn').innerHTML="Name should be greater than 3";
 		song.pause();
 		}
 		} 
+						function changeCurrentSongDetails(songobj) {			// this function define the details of the song whenever the song is paly or pause
+							$('.current-song-image').attr('src','img/' + songobj.image);
+							$('.current-song-name').text(songobj.name);
+							$('.current-song-album').text(songobj.album);
+						}
 		
 					function fancyTimeFormat(time)
 			{   
@@ -44,7 +47,13 @@ document.getElementById('errfn').innerHTML="Name should be greater than 3";
 				ret += "" + secs;
 				return ret;
 			}
-					
+				//function updateProgressbar () {
+					//var width = document.querySelector('audio');
+					//var ct = song.currentTime ;
+					//var td = song.duration ;
+					//width = (ct/td)*100;
+					//$('.progress-filled').text(width + "%");
+				//}
 		
 		
 	
@@ -53,11 +62,19 @@ document.getElementById('errfn').innerHTML="Name should be greater than 3";
 					var currentTime = Math.floor(song.currentTime);
 					currentTime = fancyTimeFormat(currentTime);
 					var duration = Math.floor(song.duration);
-					duration = fancyTimeFormat(duration)
+					duration = fancyTimeFormat(duration);
 					$('.time-elapsed').text(currentTime);
 					$('.song-duration').text(duration);
+					//updateProgressbar () ;
 				}
+				
+				function timeJump() {
+				var song = document.querySelector('audio')
+				song.currentTime = song.duration - 5;
+				}
+				
 					window.onload = function() {
+						changeCurrentSongDetails(songs[0]);    // update the 1st details of the 'songs object' when the window is loading
 					//$('#song1 .song-name').text(songlist[0]);
 					//$('#song2 .song-name').text(songlist[1]);
 					//$('#song3 .song-name').text(songlist[2]);
@@ -66,8 +83,14 @@ document.getElementById('errfn').innerHTML="Name should be greater than 3";
 					setInterval(function() {
 					updateCurrentTime();
 					},1000);
+					
+					$('#songs').DataTable({
+						paging: false
+					});
+									
 					}
-						function addSongNameClickEvent(songName,position) {
+						function addSongNameClickEvent(songobj,position) {
+							var songName = songobj.fileName;
 							var id = '#song' + position;
 						$(id).click(function() {
 						var audio = document.querySelector('audio');
@@ -77,8 +100,11 @@ document.getElementById('errfn').innerHTML="Name should be greater than 3";
 						toggleSong();
 						}
 						else {
+							
 						audio.src = songName;
 						toggleSong();
+						console.log(obj);
+						changeCurrentSongDetails(songobj);
 						}
 						});
 						}
@@ -102,28 +128,32 @@ document.getElementById('errfn').innerHTML="Name should be greater than 3";
 								'artist': 'Garry Sandhu',
 								'album': 'Jaanu',
 								'duration': '4:13',
-							   'fileName': 'song1.mp3'
+							   'fileName': 'song1.mp3',
+							   'image' : 'song1.jpg'
 							},
 							{
 								'name': 'Dont let me down',
 								'artist': 'Chainsmokers',
 								'album': 'Collage',
 								'duration': '3:28',
-								'fileName': 'song2.mp3'
+								'fileName': 'song2.mp3',
+								'image' : 'song2.jpg'
 							},
 							{
 								'name': 'My heart will go on',
 								'artist': 'Celine Dion',
 								'album': 'Titanic',
 								'duration': '5:10',
-								'fileName': 'song3.mp3'
+								'fileName': 'song3.mp3',
+								'image' : 'song3.jpg'
 							},
 							{
 								'name': 'Despacito',
 								'artist': 'Luis Fonsi',
 								'album': 'Despacito',
 								'duration': '4:41',
-								'fileName': 'song4.mp3'
+								'fileName': 'song4.mp3',
+								'image' : 'song4.jpg'
 							}]
 
 								
@@ -135,13 +165,54 @@ document.getElementById('errfn').innerHTML="Name should be greater than 3";
 							song.find('.song-artist').text(obj.artist);
 							song.find('.song-album').text(obj.album);
 							song.find('.song-length').text(obj.duration); 
-							 addSongNameClickEvent(obj.fileName,i+1)
+							 addSongNameClickEvent(obj,i+1)
 						}
-								
+						
+							$('.fa-repeat').on('click',function() {
+							$('.fa-repeat').toggleClass('disabled')
+							willLoop = 1 - willLoop;
+							});	
+												
+							$('.fa-random').on('click',function() {
+							$('.fa-random').toggleClass('disabled');
+							willShuffle = 1 - willShuffle;
+							});	
+							
+							$('audio').on('ended',function() {
+								var audio = document.querySelector('audio');
+								if (willShuffle == 1) {
+									var nextSongNumber = randomExcluded(1,4,currentSongNumber); // Calling our function from Stackoverflow
+									var nextSongobj = songs[nextSongNumber-1];
+									audio.src = nextSongobj.fileName;
+									toggleSong();
+									changeCurrentSongDetails(nextSongobj);
+									currentSongNumber = nextSongNumber;
+								}
+								else if(currentSongNumber < 4) {
+									var nextSongobj = songs[currentSongNumber];
+									audio.src = nextSongobj.fileName;
+									toggleSong();
+									changeCurrentSongDetails(nextSongobj);
+									currentSongNumber = currentSongNumber + 1;
+								}
+								else if(willLoop == 1) {
+									var nextSongobj = songs[0];
+									audio.src = nextSongobj.fileName;
+									toggleSong();
+									changeCurrentSongDetails(nextSongobj);
+									currentSongNumber =  1;
+								}
+								else {
+									$('.play-icon').removeClass('fa-pause').addClass('fa-play');
+									audio.currentTime = 0;
+								}
+							})
+							
+							
 		$('.welcome-screen button').on('click', function() {
         var name = $('#name-input').val();
         if (name.length > 2) {
-            var message = "Welcome, " + name;
+            var message = "Welcome to Songify, " + name;
             $('.main .user-name').text(message);
             $('.welcome-screen').addClass('hidden');
             $('.main').removeClass('hidden');
@@ -152,18 +223,17 @@ document.getElementById('errfn').innerHTML="Name should be greater than 3";
     $('.play-icon').on('click', function() {
         toggleSong();
     });
-    $('body').on('keypress', function(event) {
-                if (event.keyCode == 32) {
-                   toggleSong();
-				   }
-            });
-			$('.next-icon').on('click',function(){
-				 toggleSong();
+			$('body').on('keypress',function(event) {
+			var target = event.target;
+			if (event.keyCode == 32 && target.tagName !='INPUT')     // spacebar doesnot work on input field
+			{
+				toggleSong();
 			}
+		});
 			
-			$('.back-icon').on('click',function(){
-				 toggleSong();
+			$('.main button').on('click', function() {
+				$('.main').addClass('hidden');
+            $('.welcome-screen').removeClass('hidden');
+			
 			});
 			
-			
-				

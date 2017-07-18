8d41d8b1058fcd9184151fad6a08af77640e1c95
Radha <Radha4736@gmail.com>
2017-07-19 02:42:47 +0530

    images added

--- main.js
+++ main.js
@@ -262,27 +262,32 @@ changeCurrentSongDetails(songs[Playingnumber])
 
            $('.vegas-slider').vegas({
 						 slides:[
-
+							 
 							 {    src: "1.jpg" },
 							 {    src:'2.jpg'},
 							 {    src: "3.jpg" },
 							 {    src:'4.jpg'},
 							 {    src: "5.jpg" },
+							  {    src: "05.jpg" },
 							 {    src:'6.jpg'},
 							 {    src: "7.jpg" },
 							 {    src:'8.jpg'},
 							 {    src: "9.jpg" },
 							 {    src:'10.jpg'},
+							  {    src: "08.jpg" },
 							 {    src: "11.jpg" },
 							 {    src:'12.jpg'},
 							 {    src: "13.jpg" },
+							  {    src: "015.jpg" },
 							 {    src:'14.jpg'},
 							 {    src: "15.jpg" },
+							  {    src: "010.jpg" },
 							 {    src:'16.jpg'},
 							 {    src: "17.jpg" },
 							{    src:'18.jpg'},
 							{    src:'19.jpg'},
-							 {    src:'20.jpg'}
+							 {    src: "20.jpg" },
+							 {    src:'21.jpg'}
 
 						 ],
 						 animation:'kenburns'
